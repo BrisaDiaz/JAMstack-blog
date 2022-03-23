bf1234c2ca1216c8122341906ece952d241feb22
Brisa Diaz <brisaabigaildiaz2000@gmail.com>
2022-03-23 12:27:09 -0300

    setting react-query

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,21 @@
 import "../styles/globals.css";
-import type {AppProps} from "next/app";
+import React from "react";
+import type { AppProps } from "next/app";
 
-import {UserProvider} from "@auth0/nextjs-auth0";
-
+import { UserProvider } from "@auth0/nextjs-auth0";
+import { Hydrate, QueryClient, QueryClientProvider } from "react-query";
 import Layout from "@/components/layout";
-function MyApp({Component, pageProps}: AppProps) {
+function MyApp({ Component, pageProps }: AppProps) {
+  const [queryClient] = React.useState(() => new QueryClient());
   return (
     <UserProvider>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+      <QueryClientProvider client={queryClient}>
+        <Hydrate state={pageProps.dehydratedState}>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </Hydrate>{" "}
+      </QueryClientProvider>
     </UserProvider>
   );
 }
